33f027c94cbd681886711d70b3036a9a1ca68525
Nag1sa76 <vlas.lapshin05@gmail.com>
2026-03-19 16:09:56 +0300

    Было добавлено ведение логов

--- System_Of_Sklad/Logger.cs
+++ System_Of_Sklad/Logger.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace System_Of_Sklad
+namespace Sklad_System
 {
-    internal class Logger
+    public static class Logger
     {
+        private static string logFile = "actions.log";
+        private static Database db = new Database();
+
+        // Запись действия (и в БД, и в файл)
+        public static void Log(string пользователь, string действие)
+        {
+            try
+            {
+                // 1. Запись в файл
+                string запись = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {пользователь}: {действие}";
+                File.AppendAllText(logFile, запись + Environment.NewLine);
+
+                // 2. Запись в БД
+                db.ДобавитьВЖурнал(пользователь, действие);
+            }
+            catch (Exception ex)
+            {
+                // Если не можем записать - хотя бы в консоль выведем
+                Console.WriteLine($"Ошибка логирования: {ex.Message}");
+            }
+        }
+
+        // Запись ошибки
+        public static void Log(string пользователь, string действие, Exception ex)
+        {
+            Log(пользователь, $"{действие}. Ошибка: {ex.Message}");
+        }
     }
 }
