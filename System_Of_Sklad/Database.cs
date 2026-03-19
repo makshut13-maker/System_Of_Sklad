adc23b3c5c1bfc9f4b4a22020b5828655d21541d
Nag1sa76 <vlas.lapshin05@gmail.com>
2026-03-19 15:38:34 +0300

    Базовая структура и подключение к БД

--- System_Of_Sklad/Database.cs
+++ System_Of_Sklad/Database.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Dapper;
 
-namespace System_Of_Sklad
+namespace Sklad_System
 {
-    internal class Database
+    public class Database
     {
+        // ИЗМЕНИ ЭТУ СТРОКУ НА СВОЮ!
+        public string connectionString = @"Server=HUTOK_30FPS\SQLEXPRESS;Database=skla;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        // СОЗДАНИЕ ТАБЛИЦ (если их нет)
+        public void СоздатьТаблицы()
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Таблица Товары
+                conn.Execute(@"
+            IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Товары' AND xtype='U')
+            CREATE TABLE Товары (
+                Номер_товара INT PRIMARY KEY IDENTITY(1,1),
+                Название NVARCHAR(255) NOT NULL,
+                Средняя_рыночная_цена DECIMAL(18,2) NOT NULL
+            )");
+            }
+        }
     }
 }
